26c940d895ec1b8612be5a72edd260a00ed1c015
aleksposte <aleks.poste@gmail.com>
2023-08-15 21:42:37 -0400

    added example 2

---
+++ src/main/java/com/alexandr/javacore/chapter02/Example2.java
@@ -0,0 +1,14 @@
+package com.alexandr.javacore.chapter02;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num; // объявление переменной
+        num = 100; // присваивание значения
+
+        System.out.println("Это переменная num: " + num);
+
+        num = num *2;
+        System.out.print("Значение переменной num * 2 равно: ");
+        System.out.println(num);
+    }
+}
