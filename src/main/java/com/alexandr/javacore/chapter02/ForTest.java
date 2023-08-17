3fd86f00aea567fd571f074efccd099964a06a20
aleksposte <aleks.poste@gmail.com>
2023-08-16 20:37:21 -0400

    added ForTest

---
+++ src/main/java/com/alexandr/javacore/chapter02/ForTest.java
@@ -0,0 +1,11 @@
+package com.alexandr.javacore.chapter02;
+
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        // for (x = 0; x < 10; x = x + 1) // не удобная запись, в современном виде используется операция инкремента
+        for (x = 0; x < 10; x++)
+            System.out.println("Значение x: " + x);
+    }
+}
