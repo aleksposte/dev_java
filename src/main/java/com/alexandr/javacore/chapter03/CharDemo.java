ce442b4e6b54c6ce6f5cdff40429864e4a02a4a4
aleksposte <aleks.poste@gmail.com>
2023-08-16 21:38:37 -0400

    added chapter03/CharDemo

---
+++ src/main/java/com/alexandr/javacore/chapter03/CharDemo.java
@@ -0,0 +1,12 @@
+package com.alexandr.javacore.chapter03;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88; // код Х
+        ch2 = 'Y';
+
+        System.out.println("ch1 и ch2: " + ch1 + " " + ch2);
+    }
+}
