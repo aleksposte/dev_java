90be6942f94e36aa6370d3ace64253e922585cb8
aleksposte <aleks.poste@gmail.com>
2023-08-20 13:34:45 -0400

    added chapter03/AutoArray

---
+++ src/main/java/com/alexandr/javacore/chapter03/AutoArray.java
@@ -0,0 +1,9 @@
+package com.alexandr.javacore.chapter03;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        System.out.println("в апреле: " + month_days[3] + " дней.");
+    }
+}
