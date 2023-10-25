9b6ff3dce17a853280a3a4b43c9c91b3501cae93
aleksposte <aleks.poste@gmail.com>
2023-10-24 21:00:29 -0400

    task thread v2 with synchronized

---
+++ src/main/java/module_1_4_tasks/Task_1_4_v2.java
@@ -0,0 +1,74 @@
+package module_1_4_tasks;
+
+public class Task_1_4_v2 {
+    public static void main(String[] args) throws InterruptedException {
+        Foo foo = new Foo();
+
+        new Thread(() -> {
+            try {
+                foo.first(new Thread());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                foo.second(new Thread());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                foo.third(new Thread());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
+
+class Foo {
+    private boolean firstFinish = false;
+    private boolean secondFinish = false;
+    private final Object lock = new Object();
+
+    public void first(Thread thread) throws InterruptedException {
+        synchronized (lock) {
+            System.out.println("first " + Thread.currentThread().getName());
+
+            thread.run();
+            firstFinish = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void second(Thread thread) throws InterruptedException {
+        synchronized (lock) {
+            while (!firstFinish) {
+                lock.wait();
+            }
+
+            System.out.println("second " + Thread.currentThread().getName());
+
+            thread.run();
+            secondFinish = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void third(Thread thread) throws InterruptedException {
+        synchronized (lock) {
+            while (!secondFinish) {
+                lock.wait();
+            }
+
+            System.out.println("third " + Thread.currentThread().getName());
+
+            thread.run();
+            lock.notifyAll();
+        }
+    }
+}
